709bb75dab70a07adca2d3eeb17c5a2ade9699bb
aedan <113115936+FRHSDaedan@users.noreply.github.com>
2024-02-20 12:45:57 -0500

    Revert "said hello to owner and editor"
    This reverts commit 661670e101a1eff4543a7f1879396019b7bc9539.

--- firstGithub/src/Aedan_Milla.java
+++ firstGithub/src/Aedan_Milla.java
@@ -1,9 +1,5 @@
 public class Aedan_Milla {
     public static void main(String[] args) {
         System.out.println("Hi Aedan");
-        System.out.println("Hi Nasiar");
-        for(int i = 0; i < 15; i++){
-            System.out.println("I am " + i + "years old");
-        }
     }
 }
